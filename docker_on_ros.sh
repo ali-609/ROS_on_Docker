171cbc3105fd199b2710075a46bb9b09c34a902a
ali <ali@fujifedora.home.arpa>
2023-09-04 02:00:25 +0300

    Video flag

--- docker_on_ros.sh
+++ docker_on_ros.sh
@@ -3,6 +3,7 @@
 HOST_PATH=$(pwd)
 APT_LIST="python3-catkin-tools"
 GPU_FLAG=""
+VIDEO_DEVICE="/dev/video1" # Default video device
 
 # Check if apt_list.txt file exists
 if [[ -f "./apt_list.txt" ]]; then
@@ -31,6 +32,11 @@ while [[ $# -gt 0 ]]; do
       GPU_FLAG="--gpus all"
       shift
       ;;
+    --video)
+      VIDEO_DEVICE="/dev/$2"
+      shift # past argument
+      shift # past value
+      ;;
     *)    # unknown option
       shift
       ;;
@@ -42,7 +48,7 @@ docker run -it --ulimit nofile=524228:524228 --privileged $GPU_FLAG \
   --env="QT_X11_NO_MITSHM=1" \
   --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
   --volume="$HOST_PATH:/home" \
-  --device=/dev/video1:/dev/video1 \
+  --device="$VIDEO_DEVICE:$VIDEO_DEVICE" \
   --restart=always \
   --name "$CONTAINER_NAME" \
   "$DOCKER_IMAGE" \
